1690359e866c2f285201a3581abecb0d9b7bd106
PRATAPSANAP1 <pratapsanap14@gmail.com>
2025-11-16 12:45:54 +0530

    Fix CORS policy for Render deployment

--- index.js
+++ index.js
@@ -26,10 +26,24 @@ app.use(
     origin: ["https://shubhangi-collections01.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    exposedHeaders: ["Set-Cookie"]
   })
 );
-app.options('*', cors());
+
+// Handle preflight requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://shubhangi-collections01.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
